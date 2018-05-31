e10ff6cb8a9fd77f0652d948d3eadc30fc22ef03
Wil <willem.labuschagne@tvh.com>
2018-05-31 18:32:02 +0200

    List getByAuthor

--- src/main/java/za/co/tamboer/sboottut/SbootTutApplication.java
+++ src/main/java/za/co/tamboer/sboottut/SbootTutApplication.java
@@ -2,11 +2,14 @@ package za.co.tamboer.sboottut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import za.co.tamboer.sboottut.streams.BookService;
 
 @SpringBootApplication
 public class SbootTutApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbootTutApplication.class, args);
+		BookService bookService = new BookService();
+		bookService.perform();
 	}
 }
--- src/main/java/za/co/tamboer/sboottut/streams/BookService.java
+++ src/main/java/za/co/tamboer/sboottut/streams/BookService.java
@@ -2,11 +2,11 @@ package za.co.tamboer.sboottut.streams;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 //http://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/
@@ -16,36 +16,51 @@ public class BookService {
 
 
     public BookService() {
-
-        getByAuthor();
-
     }
 
-    public void getByAuthor(){
-        System.out.println("Get books by author");
+    public void perform(){
+        List<Book> myBookList = getBooks();
+        String author = "Lyall Watson";
+        getByAuthor(myBookList, author);
+        printBookByAuthor(myBookList, author);
 
-        List<Book> myBookList =
-                Arrays.asList(
-                        new Book("Spring in action", "Craig Walls"),
-                        new Book("Java 8 in action", "Uma"),
-                        new Book("Elephantoms", "Lyall Watson")
-                );
+        listToMap();
+    }
 
-        myBookList
-                .stream()
-                .filter(b -> b.byAuthor("Lyall Watson"))
-                //.map(Book::toUpperCase)
+    public List<Book> getByAuthor(List<Book> bookList, String author){
+        System.out.println("Get books by author " + author);
+
+        return bookList.stream().filter(book -> book.byAuthor(author)).collect(Collectors.toList());
+    }
+
+    private void printBookByAuthor(List<Book> bookList, String author) {
+        System.out.println("Print books by author " + author);
+
+        bookList.stream()
+                .filter(b -> b.byAuthor(author))
+                .map(b -> b.toString().toUpperCase())
                 .sorted()
                 .forEach(System.out::println);
     }
 
-    public Map <String, List<Book>> booksByAuthor(String authorName, List<Book> books){
-        Map <String, List<Book>> booksPerAuthor;
+    private List<Book> getBooks() {
+        return Arrays.asList(
+                new Book("Spring in action", "Craig Walls"),
+                new Book("Java 8 in action", "Uma"),
+                new Book("Elephantoms", "Lyall Watson")
+        );
+    }
 
-        Map <String, List<Book>> booksPerAuthor = books.stream().map(book -> book.byAuthor("Lyall Watson")).collect(Collectors.toMap(authorName,book -> book.byAuthor("Lyall Watson")));
-        Map <String, List<Book>> booksPerAuthor = books.stream().map(book -> book.byAuthor("Lyall Watson")).collect(Collectors.toMap(authorName,book -> book.byAuthor("Lyall Watson")));
+    public void listToMap(){
 
-        List<Book> booksFromWillem = booksPerAuthor.get("Lyall Watson");
+        List<String> list = new ArrayList<>();
+        list.add("Mohan");
+        list.add("Sohan");
+        list.add("Mahesh");
+        Map<String, Object> map = list.stream().collect(Collectors.toMap(Function.identity(), s->s));
+
+        map.forEach((x, y) -> System.out.println("Key: " + x +", value: "+ y));
+
     }
 
 
