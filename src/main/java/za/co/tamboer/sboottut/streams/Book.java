1f6ed3c0af8ae683d9fddf0cd67aed66ffa4c23f
Wil Labuschagne <willem.labuschagne@tvh.com>
2018-05-31 11:51:56 +0200

    add streaming package

---
+++ src/main/java/za/co/tamboer/sboottut/streams/Book.java
@@ -0,0 +1,26 @@
+package za.co.tamboer.sboottut.streams;
+
+public class Book {
+
+    private String title;
+    private String author;
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
---
+++ src/main/java/za/co/tamboer/sboottut/streams/BookService.java
@@ -0,0 +1,14 @@
+package za.co.tamboer.sboottut.streams;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+
+    public BookService() {
+    }
+
+
+
+}
