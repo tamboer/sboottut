baa461803d89edc1b4705c511cda9403f784c64b
Wil Labuschagne <willem.labuschagne@tvh.com>
2018-05-31 11:38:53 +0200

    first

---
+++ src/main/java/za/co/tamboer/sboottut/SbootTutApplication.java
@@ -0,0 +1,12 @@
+package za.co.tamboer.sboottut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootTutApplication.class, args);
+	}
+}
---
+++ src/test/java/za/co/tamboer/sboottut/SbootTutApplicationTests.java
@@ -0,0 +1,16 @@
+package za.co.tamboer.sboottut;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbootTutApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
