d1aa77f27430aba34cb9ff73abbc78814c24b3ee
Wil <willem.labuschagne@tvh.com>
2018-06-02 14:52:49 +0200

    removing componentScan and explicit use @Bean annotations with dependancy

--- src/main/java/za/co/tamboer/sboottut/config/CDPlayerConfig.java
+++ src/main/java/za/co/tamboer/sboottut/config/CDPlayerConfig.java
@@ -1,9 +1,22 @@
 package za.co.tamboer.sboottut.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import za.co.tamboer.sboottut.soundsystem.CompactDisc;
+import za.co.tamboer.sboottut.soundsystem.cdplayer.CDPlayer;
+import za.co.tamboer.sboottut.soundsystem.compactdisc.SgtPeppers;
 
 @Configuration
-@ComponentScan(basePackages = "za.co.tamboer.sboottut.soundsystem")
+//@ComponentScan(basePackages = "za.co.tamboer.sboottut.soundsystem")
 public class CDPlayerConfig {
+
+    @Bean
+    public CompactDisc sgtPeppers(){
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public CDPlayer cdPlayer(CompactDisc cd){
+        return new CDPlayer(cd);
+    }
 }
