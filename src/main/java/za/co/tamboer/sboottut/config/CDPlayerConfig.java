17f93bc863a9d907e5f273c23f4113a06d42424d
Wil <willem.labuschagne@tvh.com>
2018-06-02 15:07:08 +0200

    named beans

--- src/main/java/za/co/tamboer/sboottut/config/CDPlayerConfig.java
+++ src/main/java/za/co/tamboer/sboottut/config/CDPlayerConfig.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import za.co.tamboer.sboottut.soundsystem.CompactDisc;
 import za.co.tamboer.sboottut.soundsystem.cdplayer.CDPlayer;
+import za.co.tamboer.sboottut.soundsystem.compactdisc.ProsAndConsOfHitchHiking;
 import za.co.tamboer.sboottut.soundsystem.compactdisc.SgtPeppers;
 
 @Configuration
 //@ComponentScan(basePackages = "za.co.tamboer.sboottut.soundsystem")
 public class CDPlayerConfig {
 
-    @Bean
+    @Bean(name = "cd")
     public CompactDisc sgtPeppers(){
         return new SgtPeppers();
     }
@@ -18,5 +19,19 @@ public class CDPlayerConfig {
     @Bean
     public CDPlayer cdPlayer(CompactDisc cd){
         return new CDPlayer(cd);
+    }
+
+    @Bean(name = "randomCD")
+    public CompactDisc randomBeatlesCD() {
+        int choice = (int) Math.floor(Math.random() * 4);
+        if (choice == 0) {
+            return new SgtPeppers();
+        } else if (choice == 1) {
+            return new ProsAndConsOfHitchHiking();
+        } else if (choice == 2) {
+            return new SgtPeppers();
+        } else {
+            return new ProsAndConsOfHitchHiking();
+        }
     }
 }
